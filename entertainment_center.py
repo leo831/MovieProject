a3dde61d3cb19a58112c3f1090af04e08d2f1eac
leo831 <lelara@csumb.edu>
2016-11-23 17:09:11 -0800

    Formating changes

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,40 +1,46 @@
 import media
 import fresh_tomatoes
 
-#Warcraft movie instance
-warcraft_movie = media.Movie("Warcraft",                      
-                              "ooking to escape from his dying world, the orc shaman Gul'dan utilizes dark magic to open a portal to the human realm of Azeroth. Supported by the fierce fighter Blackhand, Gul'dan organizes the orc clans into a conquering army called the Horde. ",
-                              "http://t3.gstatic.com/images?q=tbn:ANd9GcTCzditN2MtHjUM51mMbNspnJ_NHPWYz3J8zQMoXPRtAeBTpTrf",
-                              "https://www.youtube.com/watch?v=7p43xwId1eE")
-#Suicide movie instance
-suicide_movie = media.Movie("Suicide Squade",                      
+"""
+Author: Leobardo Lara
+Title: Movie Trailer Websire
+Created: 22-Nov-2016
+"""
+
+
+# Warcraft movie instance
+warcraft_movie = media.Movie("Warcraft",
+                             "looking to escape from his dying world.",
+                             "http://t3.gstatic.com/images?q=tbn:ANd9GcTCzditN2MtHjUM51mMbNspnJ_NHPWYz3J8zQMoXPRtAeBTpTrf",
+                             "https://www.youtube.com/watch?v=7p43xwId1eE")
+# Suicide movie instance
+suicide_movie = media.Movie("Suicide Squade",
                             "Figuring they're all expendable, a U.S. intelligence officer decides to assemble a team of dangerous, incarcerated supervillains for a top-secret mission. ",
                             "http://www.gstatic.com/tv/thumb/movieposters/11319046/p11319046_p_v8_ai.jpg",
                             "https://www.youtube.com/watch?v=fyJH39ZbPAg")
 
-#Captain America movie instance
+# Captain America movie instance
 captain_america = media.Movie("Captain America: Civil War",
                               "Political pressure mounts to install a system of accountability when the actions of the Avengers lead to collateral damage. The new status quo deeply divides members of the team.",
                               "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRhP_sl7z7Pr71xPOHE2RYcedgW3JvhUEVMrkWrIyMdZnLisgJUG_Y7hw",
                               "https://www.youtube.com/watch?v=dKrVegVI0Us")
-#Batman vs Superman movie instance
+# Batman vs Superman movie instance
 batman_superman = media.Movie("Batman v Superman: Dawn of Justice",
                               "It's been nearly two years since Superman's (Henry Cavill) colossal battle with Zod (Michael Shannon) devastated the city of Metropolis.",
                               "http://t1.gstatic.com/images?q=tbn:ANd9GcS61fdKkVcQIKtObjNGAELqVwyzhwFoIfNGZVbC-rqta12xBfLa",
                               "https://www.youtube.com/watch?v=_H0Ppo41_2k")
-#The purge movie instance
+# The purge movie instance
 purge_movie = media.Movie("The Purge: Election Year",
                           "As a young woman, Sen. Charlie Roan survived the annual night of lawlessness that took the lives of her family members. As a presidential candidate, Roan is determined to end the yearly tradition of blood lust once and for all. ",
                           "https://www.uphe.com/sites/default/files/styles/scale__344w_/public/Purge3_PosterArt.jpg?itok=w3988nSV",
                           "https://www.youtube.com/watch?v=7WlUVTTervg")
-#Xmen movie instance
-xmen_movie= media.Movie("X-Men: Apocalypse",
-                        "X-Men: Apocalypse is a 2016 American superhero film based on the fictional X-Men characters that appear in Marvel Comics. It is the ninth installment in the X-Men film series and a sequel to X-Men: Days of Future Past.",
-                        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGGUCnZUo21NneIsqAXG1-BpyiXiZvVovv8ju4jBtPjPY18iDFOQrFEA",
-                        "https://www.youtube.com/watch?v=kFXV-9jftLk")
+# Xmen movie instance
+xmen_movie = media.Movie("X-Men: Apocalypse",
+                         "X-Men: Apocalypse is a 2016 American superhero film based on the fictional X-Men characters that appear in Marvel Comics. It is the ninth installment in the X-Men film series and a sequel to X-Men: Days of Future Past.",
+                         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGGUCnZUo21NneIsqAXG1-BpyiXiZvVovv8ju4jBtPjPY18iDFOQrFEA",
+                         "https://www.youtube.com/watch?v=kFXV-9jftLk")
 
-#create an array to store movies instances.
+# create an array to store movies instances.
 movies = [warcraft_movie, suicide_movie, captain_america, batman_superman, purge_movie, xmen_movie]
-
-#send movie array information to the open_movie_page method from fresh_tomatoes.
+# send movie array information to the open_movie_page method from fresh_tomatoes.
 fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -1,16 +1,20 @@
 import webbrowser
 
+"""
+Author: Leobardo Lara
+Title: Movie Trailer Websire
+Created: 22-Nov-2016
+"""
+
+
+# Cunstructor
 class Movie():
-    """This  class provides a way to store movie related information"""
-    VALID_RATINGS = ["G", "Pg", "PG-13", "R"]
-    
+
+    # Method
     def __init__(self, movie_title, movie_storyline, poster_image,
                  trailer_youtube):
+        # instance variables
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
-
-    def show_trailer(self):
-        webbrowser.open(self.trailer_youtube_url)
-        
